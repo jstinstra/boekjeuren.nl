5855842ccc0783a2741f1bb99d38ea9a833e5fce
ThaSmokey(Joshua) <235222@cursist.rockopnh.nl>
2016-12-06 10:52:17 +0100

    Removed database connection and added connect.php to the file checkbox.php

--- checkbox.php
+++ checkbox.php
@@ -1,20 +1,13 @@
 <?php
 	session_start();
 	include("urenboekadmin.php");
-	//Vars VOOR DATABASE
-	$server = "localhost";
-	$user = "root";
-	$password = "usbw";
-	$db = "urenboek";
 
-	
+	require('connect.php');
+
 	//VARS VOOR SQL CODE
 	$User = $_SESSION['Username'];
 	$id = $_POST['PostID'];
 	
-	//Database verbinding
-	mysql_connect($server, $user, $password) or die("Geen verbinding met database mogelijk");
-	mysql_select_db($db) or die ("Database niet gevonden");
 	$query1 =  mysql_query("SELECT `PostID`, `User`, `Tijd-in`, `Tijd-uit`, `Datum`, `Locatie`, `Vak`, `Check`, `Werkzaamheden`, `Opmerking` FROM `posts` WHERE `PostID` = $id AND `Check` = 1;") or die ("Fout met zoeken");
 	$query2 =  mysql_query("SELECT `PostID`, `User`, `Tijd-in`, `Tijd-uit`, `Datum`, `Locatie`, `Vak`, `Check`, `Werkzaamheden`, `Opmerking` FROM `posts` WHERE `PostID` = $id AND `Check` = 2;") or die ("Fout met zoeken");
 	$query0 =  mysql_query("SELECT `PostID`, `User`, `Tijd-in`, `Tijd-uit`, `Datum`, `Locatie`, `Vak`, `Check`, `Werkzaamheden`, `Opmerking` FROM `posts` WHERE `PostID` = $id AND `Check` = 0;") or die ("Fout met zoeken");
@@ -22,12 +15,12 @@
 	$result2 = mysql_num_rows($query2);
 	$result0 = mysql_num_rows($query0);
 
-	
+
 	if ($result0 == 1){
 	$sql =  mysql_query("SELECT `PostID`, `User`, `Tijd-in`, `Tijd-uit`, `Datum`, `Locatie`, `Vak`, `Check`, `Werkzaamheden`, `Opmerking` FROM `posts` WHERE `PostID` = $id AND `Check` = 0;");
 	mysql_query($sql);
-	$goed = mysql_num_rows($sql);	
-	
+	$goed = mysql_num_rows($sql);
+
 	if($goed == 1){
 	$sql =  mysql_query("UPDATE `urenboek`.`posts` SET `Check` = '1' WHERE `posts`.`PostID` = $id;");
 		echo"<div class='alert alert-success' style='text-align: center;'>Goedkeuring goedgekeurd</div>";
@@ -40,12 +33,12 @@
 	}
 	}
 
-	
+
 	if ($result1 == 1){
 	$sql1 =  mysql_query("SELECT `PostID`, `User`, `Tijd-in`, `Tijd-uit`, `Datum`, `Locatie`, `Vak`, `Check`, `Werkzaamheden`, `Opmerking` FROM `posts` WHERE `PostID` = $id AND `Check` = 1;");
 	mysql_query($sql1);
-	$goed2 = mysql_num_rows($sql1);	
-	
+	$goed2 = mysql_num_rows($sql1);
+
 	if($goed2 == 1){
 	$sql1 =  mysql_query("UPDATE `urenboek`.`posts` SET `Check` = '2' WHERE `posts`.`PostID` = $id;");
 
@@ -55,7 +48,7 @@
 		echo"er is iets fout gegaan";
 	}
 	}
-	
+
 	if ($result2 == 1){
 	$sql2 =  mysql_query("SELECT `PostID`, `User`, `Tijd-in`, `Tijd-uit`, `Datum`, `Locatie`, `Vak`, `Check`, `Werkzaamheden`, `Opmerking` FROM `posts` WHERE `PostID` = $id AND `Check` = 2;");
 	mysql_query($sql2);
@@ -69,5 +62,5 @@
 		echo"er is iets fout gegaan";
 	}
 	}
-	
+
 ?>
