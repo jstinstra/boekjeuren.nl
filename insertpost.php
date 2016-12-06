9d0ce3e169817048ce4d18691d4004a8a029b8eb
Bob <Bob.Wezelman@hotmail.com>
2016-12-06 13:53:36 +0100

    mysql_query -> mysqli_query

--- insertpost.php
+++ insertpost.php
@@ -16,7 +16,7 @@
 
 	//Gegevens invoeren in database
 	$QUERY = "INSERT INTO `posts`(`PostID`, `User`, `Tijd-in`, `Tijd-uit`, `Datum`, `Locatie`, `Vak`, `Werkzaamheden`) VALUES (NULL, '$User', '$TimeStart' ,'$TimeStop', '$Date','$Locatie', '$Vak', '$Werkzaamheden');" ;
-	mysql_query($QUERY) or die ("Mislukt om gegevens toe te voegen. query is:<br> " .$QUERY);
+	mysqli_query($conn, $QUERY) or die ("Mislukt om gegevens toe te voegen. query is:<br> " .$QUERY);
 
 	echo("<div class='saved'>");
 	echo("Het is gelogged! Klik op view om het te zien");
