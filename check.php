785b803c3cf611268d0bf514956753f7953f63b5
Bob <Bob.Wezelman@hotmail.com>
2016-12-06 11:30:44 +0100

    check.php ook aangepast op mysqli

--- check.php
+++ check.php
@@ -4,16 +4,18 @@ $Leerlingnummer = $_SESSION['Username'];
 
 require('connect.php');
 
-$PHP = mysql_query("SELECT * FROM account WHERE Leerlingnummer='$Leerlingnummer' AND FirstLogin= 0;");
-$Telquery = mysql_num_rows($PHP);
+$query = "SELECT * FROM account WHERE Leerlingnummer='$Leerlingnummer' AND FirstLogin= 0";
+$PHP = mysqli_query($conn, $query);
+$Telquery = mysqli_num_rows($PHP);
 
 if($Telquery == 1){
  session_start();
  $_SESSION['Username'] = $Leerlingnummer;
  //$ADMINQuery = mysql_query("SELECT * FROM account WHERE Leerlingnummer='$Leerlingnummer' AND Wachtwoord='$Encrypt'")
  //header("Location: Tabel.php");
- $sql = mysql_query("SELECT * FROM account WHERE Leerlingnummer='$Leerlingnummer' AND FirstLogin= 0");
- $Telquery2 = mysql_num_rows($sql) or die ("JE VIND ME NOOIT!");
+ $query1 = "SELECT * FROM account WHERE Leerlingnummer='$Leerlingnummer' AND FirstLogin= 0";
+ $sql = mysqli_query($conn, $query1);
+ $Telquery2 = mysqli_num_rows($sql) or die ("JE VIND ME NOOIT!");
  if($Telquery2 == 1){
   header("Location: wachtwoord.php");
  }else{
